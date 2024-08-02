2e1c0f680b87ff928806cf48f34c89002f007c23
codemaxx1 <77362054+codemaxx1@users.noreply.github.com>
2024-08-02 10:11:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -312,9 +312,10 @@ class Terandelle:
         # Reading Microphone as source
         # listening the speech and store in audio_text variable
         with sr.Microphone() as source:
-        print("Talk")
-        audio_text = r.listen(source)
-        print("Time over, thanks")
+            print("Talk")
+            audio_text = r.listen(source)
+            print("Time over, thanks")
+            
         # recoginze_() method will throw a request
         # error if the API is unreachable,
         # hence using exception handling
