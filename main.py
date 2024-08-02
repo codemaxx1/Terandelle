30cdf650df786ba110879d7e904fbec19e7ba4e8
codemaxx1 <77362054+codemaxx1@users.noreply.github.com>
2024-08-02 10:10:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -306,6 +306,25 @@ class Terandelle:
         :param display: instance of display class
         :return: the text-converted voice data
         '''
+        # Initialize recognizer class (for recognizing the speech)
+        r = sr.Recognizer()
+        
+        # Reading Microphone as source
+        # listening the speech and store in audio_text variable
+        with sr.Microphone() as source:
+        print("Talk")
+        audio_text = r.listen(source)
+        print("Time over, thanks")
+        # recoginze_() method will throw a request
+        # error if the API is unreachable,
+        # hence using exception handling
+        
+        try:
+            # using google speech recognition
+            print("Text: "+r.recognize_google(audio_text))
+        except:
+            print("Sorry, I did not get that")
+            
         while True:
             print('looping on listen')
             time.sleep(1)
