7ac5e7b77969ed81f4d681484e54b7c5257d20ac
codemaxx1 <garrnic3@isu.edu>
2024-08-02 20:37:00 -0600

    update of self program

--- main.py
+++ main.py
@@ -365,8 +365,8 @@ class Display:
         i2c = board.I2C()  # uses board.SCL and board.SDA
         display_bus = I2CDisplayBus(i2c, device_address=0x3C, reset=oled_reset)
 
-        self.displayWidth = 128
-        self.displayHeight = 64
+        self.displayWidth = 64
+        self.displayHeight = 128
         BORDER = 5
 
         self.display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=self.displayWidth, height=self.displayHeight, rotation=90)
