5107825c5e4ec25958eeab97ce98c176da9dbe49
codemaxx1 <garrnic3@isu.edu>
2024-08-08 01:31:54 -0600

    update of self program

--- textProcessingClass.py
+++ textProcessingClass.py
@@ -80,6 +80,9 @@ class TextProcessing:
                     commandToRun = token
                     print(f"command to run is {token}")
 
+        if properNouns == []:
+            properNouns = nouns.strip(commandToRun)
+
         print(f"actions to be performed: {actions}\nnouns captures: {nouns}\nproper noun captures: {properNouns}")
         print(f"located from files -- {locatedFromFiles}")
 
