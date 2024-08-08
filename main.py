1d2fe653763d90940cccd8a0bd5f5c1c68962d4c
codemaxx1 <garrnic3@isu.edu>
2024-08-08 00:55:58 -0600

    update of self program

--- main.py
+++ main.py
@@ -4,29 +4,19 @@
 """
 
 #imports
-
-import feedparser               # for news RSS
 import threading
-import re                       # used for regular expressions
-import wikipedia                # used in searching wikipedia data
 import subprocess
-import datetime                 # used in time function
 import sys
-import psutil
-import requests                 # used to send data requests
-import json
 import time
 import speech_recognition as sr # for speech recognition
-#import spacy                    # for natural language processing
 from gtts import gTTS           # for tts
 import vlc
-#from playsound import playsound # to play TTS .mp3 file after it is generated
 
 # custom classes
 from displayClass import Display
 from personClass import Person
 from textProcessingClass import TextProcessing
-
+from executeClass import ExecuteClass
 
 '''
     Terandelle class
@@ -76,205 +66,6 @@ class Terandelle:
         user = "Ira"
         return user
 
-
-    def getBatteryInformation(self):
-        '''
-        get the battery information for the system
-        :return: battery info
-        '''
-        # get battery data
-        batteryInfo = psutil.sensors_battery()
-        # print("Battery percentage : ", battery.percent)
-        # print("Power plugged in : ", battery.power_plugged)
-        # converting seconds to hh:mm:ss
-        # print("Battery left : ", convertTime(battery.secsleft))
-        return batteryInfo
-
-
-    def update(self):
-        '''
-        update Terandelle program from git repo
-        :return: return of subprocess call for update
-        '''
-        #self.say("pulling from remote repo")
-
-        returnedValue = [0,0]
-        returnedValue[0] = subprocess.check_output(["git", "pull"])
-
-        #self.say(returnedValue.decode("utf-8"))
-
-        self.say("update of program complete. Now, I'm updating Python3 packages")
-
-        updatePackages = "python3 library_installer.py"
-        returnedValue[1] = subprocess.call(str(updatePackages), shell=True)
-
-        self.say("update of python3 packages complete. Updating text processing datasets and models")
-        TextProsessing.update()
-
-        return returnedValue
-
-
-    def restartProgram(self):
-        '''
-        restart program by creating a new instance of it and terminating this one
-        :return: none
-        '''
-        self.say("restarting program")
-        openNewProcess = "python3 " + str(sys.argv[0])
-        subprocess.call(str(openNewProcess), shell=True)    # open new instance of self
-        sys.exit()                                          # terminate instance of self
-
-
-    def getDateTime(self):
-        '''
-        return the current datetime data in the requested format
-        :return: datetime data
-        '''
-        now = datetime.datetime.now()
-        dateTimeData = {"second":now.second,
-                        "minute" : now.minute,
-                        "hour" : now.hour,
-                        "day" : now.day,
-                        "month" : now.month,
-                        "year" : now.year,
-                        }
-
-        return dateTimeData
-
-
-    def wikipediaDefine(self, query, lineStart, lineEnd):
-        '''
-        parse the first bit of the wikipedia article about input:query
-        :param query: the thing you want to know about
-        :param lineStart: the position in the text that you want to start reading from (typically zero)
-        :param lineEnd: the position in the text that you want to stop reading at
-        :return:
-        '''
-        self.say("let's see what I can find")
-        try:
-            text = wikipedia.summary(str(query))
-            defineTextList = text.split('.')
-
-            # loop through input:lines, or fewer lines in the return if there are fewer than input:lines lines in it
-            for i in range( lineStart, min(len(defineTextList) + 1, lineEnd) ):
-                print("line: " + str(i))
-                self.say(defineTextList[i])
-        except Exception as e:
-            self.say(f"failure finding information about {query} because of {e}, could you try again?")
-
-
-    def getNews(self):
-        '''
-        parse the yahoo news rss feed for information about the world today
-        :return: newsfeed data
-        '''
-        d = feedparser.parse('https://www.yahoo.com/news/rss')
-        newsFeedList = []
-        for post in d.entries:
-            newsFeedList.append(post.title)
-        return newsFeedList
-
-
-    def news(self, Display):
-        '''
-        get you your news
-        :return: 1 on completion
-        '''
-        newsArray = self.getNews()
-
-
-        return 1
-
-
-    def getWeather(self, city):
-        '''
-        return the weather data for a specific city
-        :param city: the name of a city
-        :return: 1 on success
-        '''
-        apiKey = '75f6c15e22433ed3e7406b24691a0706'                         # api key (might be outdated)
-        baseURL = "http://api.openweathermap.org/data/2.5/weather?"         # url to get data from
-        url = baseURL + "appid=" + apiKey + "&q=" + city
-
-        # send data request and get JSON
-        requestResponse = requests.get(url)
-        jsonResponse = requestResponse.json()
-        print(jsonResponse)
-        # if city is found
-        if jsonResponse["cod"] != "404":
-
-            # define weather variables
-            self.temperature = jsonResponse["main"]["temp"]
-            self.pressure = jsonResponse["main"]["pressure"]
-            self.humidity = jsonResponse["main"]["humidity"]
-            self.feelsLike = jsonResponse["main"]["feels_like"]
-            self.tempMin = jsonResponse["main"]['temp_min']
-            self.tempMax = jsonResponse["main"]['temp_max']
-            self.coordinates = jsonResponse['coord']
-            self.visibility = jsonResponse['visibility']
-            self.windSpeed = jsonResponse['wind']['speed']
-            self.windDirection = jsonResponse['wind']['deg']
-            self.clouds = jsonResponse['clouds']
-            self.name = jsonResponse['name']
-            self.weather = jsonResponse["weather"]
-            self.weatherDescription = self.weather[0]["description"]
-            self.main = self.weather[0]['main']
-
-            # convert units from kelvin to Farenheight
-            self.temperature = (9 / 5) * (self.temperature - 273) + 32
-
-        else:
-            self.say(f"i'm sorry, but {city} does not seem to exist.")
-
-            """
-            example of returned weather JSON
-            {   'coord': {'lon': -111.7897, 'lat': 43.826}, 
-                'weather': [{
-                    'id': 801, 
-                    'main': 'Clouds', 
-                    'description': 'few clouds', 
-                    'icon': '02n'}], 
-                'base': 'stations', 
-                'main': {
-                    'temp': 263.05, 
-                    'feels_like': 258.83, 
-                    'temp_min': 261.88, 
-                    'temp_max': 274.74, 
-                    'pressure': 1011, 
-                    'humidity': 79}, 
-                'visibility': 10000, 
-                'wind': {'speed': 2.06, 'deg': 50}, 
-                'clouds': {'all': 20}, 
-                'dt': 1641263617, 
-                'sys': {
-                    'type': 1, 
-                    'id': 5735, 
-                    'country': 'US', 
-                    'sunrise': 1641222077, 
-                    'sunset': 1641254509}, 
-                'timezone': -25200, 
-                'id': 5605242, 
-                'name': 'Rexburg', 
-                'cod': 200}
-            """
-        return 1
-
-
-    def weather(self, city, Display):
-        weather = self.getWeather(city)
-        # build TTS response
-        response = "In " + str(city) + "the weather is " + str(weather.weatherDescription) + " and it is " + str(
-            round(weather.temperature ) ) + " degrees"
-        if weather.windSpeed > 10:
-            response += " and wind speed is " + str(weather.windSpeed) + " so it feels like " + str(weather.feelsLike)
-        # Temperature (in kelvin unit) = " + str(temperature) + " atmospheric pressure (in hPa unit) = " + str(pressure) + " humidity (in percentage) = " + str(humidity) + " description = " +
-
-        Display.printText(f"weather : {weather.weatherDescription}", 0, 0, 255)
-        Display.printText(f"temp : {weather.temperature}", 0, "1textHeight", 255)
-
-        self.say(response)
-
-        return 1
 
 
     def bootup(self, Display):
@@ -333,7 +124,25 @@ class Terandelle:
             Display.printText(0, 10, f"\"{command}\"", 1)
 
 
-            functionRun = TextProcessing.recognizeIntent(TextProcessing.analyzeCommand(command))
+            properNoun = ""
+            functionRun, properNounList = TextProcessing.recognizeIntent(TextProcessing.analyzeCommand(command))
+            for i in properNounList:
+                properNoun = properNoun + " " + str(i)
+
+            if functionRun == "update":
+                Execute.update()
+            elif functionRun == "restart":
+                Execute.restartProgram()
+            elif functionRun == "news":
+                Execute.news(Display)
+            elif functionRun == "time":
+                Execute.getDateTime()
+            elif functionRun == "wikipedia":
+                Execute.wikipediaDefine("thing to query", 0, 3)
+            elif functionRun == "weather":
+                Execute.weather(properNoun, Display)
+
+
 
             # update the image buffer
             Display.updateScreen()
@@ -352,6 +161,9 @@ if __name__ == "__main__":
 
     print("init terandelle")
     Terandelle = Terandelle(Display)
+
+    print('init execute')
+    Execute = ExecuteClass()
 
     #print("update")
     #Terandelle.update()
